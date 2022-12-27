59e6a82dd2280ee2075fc6b0dc1f9c99813a6ef2
Bank <sirawit.36298@gmail.com>
2022-12-27 15:42:58 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double l,p,y;
 	cout << "Enter initial loan: ";
+	cin >> l;
 	cout << "Enter interest rate per year (%): ";
+	cin >> p;
 	cout << "Enter amount you can pay per year: ";
+	cin >> y;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +22,29 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	double eoy,pb,i,t,pay,nb;
+	pb = l, eoy = 1;
+	while(pb>0){
+		cout << fixed << setprecision(0);
+		cout << setw(13) << left << eoy;
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << pb;
+		i = pb*(p/100.00);
+		cout << setw(13) << left << i;
+		t = pb+i;
+		cout << setw(13) << left << t;
+		if(t<y){
+			cout << setw(13) << left << t;
+			pay = t;
+		}else{
+			cout << setw(13) << left << y;
+			pay = y;
+		}
+		nb = t-pay;
+		cout << setw(13) << left << nb;
+		pb = nb;
+		cout << "\n";
+		eoy++;
+	}
 	return 0;
 }
