e2b8df1c111fd1afc47cfdf8e440d6924b94eea4
Bank <sirawit.36298@gmail.com>
2022-12-27 15:58:04 +0700

    lab10_2 completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -7,11 +7,11 @@ void printO(int N,int M){
 		cout << "Invalid input";
 	}else{
 		for(int i = 0; i < N; i++){
-		for(int j = 0;j < M; j++){
-			cout << "O";
+			for(int j = 0;j < M; j++){
+				cout << "O";
+			}
+			cout << '\n';
 		}
-		cout << '\n';
-	}
 	}
 }
 
